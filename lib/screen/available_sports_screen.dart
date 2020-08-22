bf6faec89f66f605161103e421b577ea2ee03bf3
Atri Das <babinsamrat123@gmail.com>
2020-08-22 12:51:25 +0530

    creates the league UI

--- lib/screen/available_sports_screen.dart
+++ lib/screen/available_sports_screen.dart
@@ -57,7 +57,7 @@ class AvailableSportsScreen extends StatelessWidget {
             child: Container(
               child: ListView.builder(
                 physics: BouncingScrollPhysics(),
-                itemBuilder: (_,int index) => SportsDetailsModel(),
+                itemBuilder: (_, int index) => SportsDetailsModel(),
                 itemCount: 5,
               ),
             ),
@@ -76,10 +76,78 @@ class SportsDetailsModel extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(10.0),
       child: Container(
-        height: height * 0.2,
-        child: Image.asset('images/soccer.jpg',fit: BoxFit.cover,),
+        height: height * 0.25,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage('images/soccer.jpg'),
+          ),
+        ),
+        child: Padding(
+          padding: EdgeInsets.only(
+            top: 15.0,
+            bottom: 10.0,
+            left: 15.0,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'American Major League Soccer',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                  fontSize: width * 0.045,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 10.0,
+                  vertical: 10.0,
+                ),
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Container(
+                    height: 25.0,
+                    width: 110.0,
+                    color: Colors.amber,
+                  ),
+                ),
+              ),
+              Row(
+                children: [
+                  ColorFiltered(
+                    colorFilter: ColorFilter.mode(
+                      Colors.white,
+                      BlendMode.saturation,
+                    ),
+                    child: Image.asset(
+                      'images/twitter.png',
+                      width: width * 0.08,
+                      colorBlendMode: BlendMode.lighten,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  ColorFiltered(
+                    colorFilter: ColorFilter.mode(
+                      Colors.white,
+                      BlendMode.saturation,
+                    ),
+                    child: Image.asset(
+                      'images/facebook.png',
+                      width: width * 0.08,
+                      colorBlendMode: BlendMode.lighten,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
 }
-
