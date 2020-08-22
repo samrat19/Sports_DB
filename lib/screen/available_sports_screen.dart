129eea353d26c1f5d91c178c1a8523f826c7a3e6
Atri Das <babinsamrat123@gmail.com>
2020-08-22 12:26:23 +0530

    adds image in sports screen

--- lib/screen/available_sports_screen.dart
+++ lib/screen/available_sports_screen.dart
@@ -45,9 +45,41 @@ class AvailableSportsScreen extends StatelessWidget {
                 ),
               ),
             ),
-          )
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Container(
+              height: height * 0.07,
+              color: Colors.amber,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              child: ListView.builder(
+                physics: BouncingScrollPhysics(),
+                itemBuilder: (_,int index) => SportsDetailsModel(),
+                itemCount: 5,
+              ),
+            ),
+          ),
         ],
       ),
     );
   }
 }
+
+class SportsDetailsModel extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Container(
+        height: height * 0.2,
+        child: Image.asset('images/soccer.jpg',fit: BoxFit.cover,),
+      ),
+    );
+  }
+}
+
